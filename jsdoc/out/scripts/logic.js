23b9375ebfec839119a1c2de7876b38b512ce38c
duri <juraj.zittnansky@adaptiware.com>
2022-04-12 20:54:24 +0200

    Updated README.md

--- jsdoc/out/scripts/logic.js
+++ jsdoc/out/scripts/logic.js
@@ -3,7 +3,7 @@
  * @constructor
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
- * @param {string} MOXRE - The MOXRE of the book.
+ * @param {string} XXXXXXXXXXx - The XXXXXXXXXXx of the book.
  */
- function Book(title, author, MOXRE) {
+ function Book(title, author, XXXXXXXXXXx) {
 }
