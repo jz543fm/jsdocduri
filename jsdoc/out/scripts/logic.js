2b0eee3af8f226267c749402836e856d7bf0f4da
duri <juraj.zittnansky@adaptiware.com>
2022-04-12 20:46:49 +0200

    initial commit

---
+++ jsdoc/out/scripts/logic.js
@@ -0,0 +1,9 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * @param {string} MOXRE - The MOXRE of the book.
+ */
+ function Book(title, author, MOXRE) {
+}
